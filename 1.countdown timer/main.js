69e7a19444826ee7f969dbc39b410290d9d1ef4e
yujiseok <koesijuy@urbanisom.com>
2022-03-15 18:01:42 +0900

    first commit

---
+++ 1.countdown timer/main.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = '1 Jan 2023';
+
+function countdown() {
+  const newYearsDate = new Date(newYears); // 2023.01.01
+  const currentDate = new Date(); // 현재
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000; // 새해 - 현재 = 밀리세컨드,  밀리세컨드 / 1000
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
